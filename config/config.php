3b4fc1be4d273f889dd3e2cd4c97d895e12a20f0
nabilbout21 <62855614+nabilbout21@users.noreply.github.com>
2021-11-19 14:02:29 -0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php  
+// ================================================================= //
+// ================================================================= //
+// =============== By Mohammed Cha : Re-skinning GRP =============== //
+// ================================================================= //
+// ================================================================= //
+
+$uri = 'https://www.sitedialek.com';
+
+$wname = 'smia dial site dialek'; 
+
+$descrip = 'description dial site dialek';  
+
+$cpa_link = 'https://contentlockerdialek.com';  
+
+$ImdbApi = 'a913ee104db6b795d20852a9ed989036';
+
+$comingsoon = '1';  // 1 to display coming soon or 0 to hide 
+$related = '1'; // 1 to display related movies or 0 to hide 
+
+?>
